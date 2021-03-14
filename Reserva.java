9ce6281d7a88fd3d46417397c0d701cd3d387ec4
Lorenalgm <lorena@proesc.com>
2021-03-13 23:00:04 -0300

    Criados campos e get/set de reservas

--- Reserva.java
+++ Reserva.java
@@ -1,5 +1,28 @@
 package airlines;
 
-public class Reserva extends Passageiro {
+public class Reserva {
+	Voo voo;
+	Passageiro passageiro;
+	
+	public Reserva(Voo voo, Passageiro passageiro) {
+		this.voo = voo;
+	    this.passageiro = passageiro;
+	}
+	
+	public Voo getVoo() {
+	    return voo;
+	}
+
+	public void Voo(Voo voo) {
+	   this.voo = voo;
+	}
+
+	public Passageiro getPassageiro() {
+	    return passageiro;
+	}
+
+	public void setPassageiro(Passageiro passageiro) {
+	   this.passageiro = passageiro;
+	}
 
 }
