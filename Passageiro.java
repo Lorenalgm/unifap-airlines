651c8e3a163bcb5952f37ce8532b1fd03bec111d
Lorenalgm <lorena@proesc.com>
2021-03-13 22:34:50 -0300

    Criados métodos na classe de passageiros

--- Passageiro.java
+++ Passageiro.java
@@ -1,5 +1,49 @@
 package airlines;
 
-public class Passageiro extends Voo {
+public class Passageiro {
+	String nome;
+	String cpf;
+	String email;
+	String celular;
+
+	public Passageiro(String nome, String cpf, String email, String celular) {
+	   this.nome = nome;
+	   this.cpf = cpf;
+	   this.email = email;
+	   this.celular = celular;
+	}
+
+	public String getNome() {
+	   return nome;
+	}
+
+	public void setNome(String nome) {
+	   this.nome = nome;
+	}
+	
+	public String getCpf() {
+	   return cpf;
+	}
+
+	public void setCpf(String cpf) {
+	   this.cpf = cpf;
+	}
+		
+	public String getEmail() {
+	   return email;
+	}
+
+	public void setEmail(String email) {
+	   this.email = email;
+	}
+			
+	public String getCelular() {
+	   return celular;
+	}
+
+	public void setCelular(String celular) {
+	   this.celular = celular;
+	}
+	 
 
 }
